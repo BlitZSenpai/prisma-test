62bf24d6edc94974a647d545bbf84cbd85fe6acb
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-22 21:24:47 +0530

    add: different operations

--- script.ts
+++ script.ts
@@ -3,7 +3,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.use;
+  const user = await prisma.user.findMany({
+    where: {
+      name: "rich dude",
+    },
+    distinct: ["name", "age"],
+    take: 2,
+    skip: 1,
+    orderBy: {
+      age: "asc",
+    },
+  });
+
+  console.log(user);
 }
 
 main()
